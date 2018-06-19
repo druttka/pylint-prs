1d487ac7fa0e0df984596d2574d84d5822b56ea0
David Ruttka <david.ruttka@microsoft.com>
2018-06-19 09:35:54 -0700

    Remove docstring to make lint fail

--- pylintprs/hello.py
+++ pylintprs/hello.py
@@ -1,5 +1,4 @@
 """The hello module for testing pylint and tox integration in PRs"""
 
 def get_greeting(name):
-    """Returns a formatted greeting given a name"""
     return 'Hello {0}'.format(name)
