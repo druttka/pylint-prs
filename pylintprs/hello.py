daca1bc61e5249ae15f0879d8ea402e072242249
David Ruttka <david.ruttka@microsoft.com>
2018-06-19 08:59:56 -0700

    Add pylint. Next step - PR / Travis integration

--- pylintprs/hello.py
+++ pylintprs/hello.py
@@ -1,2 +1,5 @@
+"""The hello module for testing pylint and tox integration in PRs"""
+
 def get_greeting(name):
+    """Returns a formatted greeting given a name"""
     return 'Hello {0}'.format(name)
